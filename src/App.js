5490c8adafb66efeecafe4df61713ea80eab7236
fancn21th <fancn21th@gmail.com>
2022-08-19 11:16:20 +0800

    a resource of users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 // in src/App.js
 import * as React from "react";
-import { Admin } from "react-admin";
+
+import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
