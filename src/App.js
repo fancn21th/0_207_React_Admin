7c9d38826b1c780998e26b22137088a632f4e5d6
fancn21th <fancn21th@gmail.com>
2022-08-19 11:59:00 +0800

    relations in datagrid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // in src/App.js
 import * as React from "react";
-import { UserList } from "./users";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { PostList } from "./posts";
+import { UserList } from "./users";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
